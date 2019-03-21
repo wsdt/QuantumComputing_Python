a794cdafe1d7ba325d2d8922538e11c59f4cf196
wsdt <kevin.riedl.privat@gmail.com>
2019-03-21 18:05:42 +0100

    docker-compose, docker and quantum dice work

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,14 @@
 FROM continuumio/miniconda3
 
+# (c) Riedl A. Kevin, 2019
+# https://github.com/wsdt
+# https://www.linkedin.com/in/kevin-riedl-947219158
+MAINTAINER Riedl A. Kevin
+
 RUN conda install -c rigetti pyquil
 
+# Copy source files into image
 COPY ./src ./src
 
+# Execute main-file
 CMD python ./src/qbit_love.py
---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# (c) Riedl A. Kevin, 2019
+# https://github.com/wsdt
+# https://www.linkedin.com/in/kevin-riedl-947219158
+
+# This file executes the docker-compose file to start the quantum virtual machine as well as the
+# quantum application.
+
+docker-compose up --rm qvirtual_machine qapplication
