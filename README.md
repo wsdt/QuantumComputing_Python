1f78e73af1fa96d2f2454d39d8d7866380a68fb5
wsdt <kevin.riedl.privat@gmail.com>
2019-03-22 16:03:56 +0100

    readme, string generator works

--- README.md
+++ README.md
@@ -1,16 +1,38 @@
 # Quantum Computing with Python
-Exemplary Python3-Programs programmed for quantum computers.
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-no-red.svg)](https://bitbucket.org/lbesson/ansi-colors) 
+[![Generic badge](https://img.shields.io/badge/Docker-Compatible-blue.svg)](https://docker.com)
+[![Github license](https://github.com/wsdt/QuantumComputing_Python.svg)](https://github.com/wsdt/QuantumComputing_Python/blob/master/LICENSE)
+
+I basically just played around. 
+
+## Programs
+This repo contains following programs. 
+
+### Quantum Dice
+The source-folder contains a program for a quantum dice which has more sides than usual. 
+This program uses the Rigetti Api/VM to execute the program.   
+
+#### Run
+To run e.g. quantum_dice.py which uses the rigetti api, you can use the 
+Docker-Compose-File like this:
+ 
+`docker-compose up --build rigetti_qapp rigetti_qvm`
+
+
+### Quantum String Generator (Real Randomness)
+This program generates a truly randomized string via QBits. You can easily change the length of the
+string in the python-file. 
+
+#### Run
+If you want the qiskit-applications like _quantum_true_randomstring_generator.py_ 
+you can use the following command: 
+
+`docker-compose up --build qiskit_qapp` 
 
 ## Docker
 I strongly recommend you to use the Docker images/Docker-compose file. 
 It avoids a lot of pain and headache :)
 
-
-## Run
-To run e.g. quantum_dice.py which uses the rigetti api, you can use the 
-Docker-Compose-File like this: 
-`docker-compose up --build rigetti_qapp rigetti_qvm`
-
-If you want the qiskit-applications like _quantum_true_randomstring_generator.py_ 
-you can use the following command: 
-`docker-compose up --build qiskit_qapp` 
+I provided a Dockerfile for both programs as they use different dependencies, libraries and 
+even APIs. For that reason I created a docker-compose file too, as e.g. the Rigetti VM depends
+on another container. 
--- src/quantum_true_randomstring_generator.py
+++ src/quantum_true_randomstring_generator.py
@@ -2,18 +2,21 @@
 # https://github.com/wsdt
 # https://www.linkedin.com/in/kevin-riedl-947219158
 
+# Inspired from https://github.com/robbiemccorkell/quantum-random-number
+
 import math, argparse, warnings
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister, execute, BasicAer
 
 MAX_QUBITS = 5
 
 
-def get_register_sizes(n, max_qubits):
-    register_sizes = [max_qubits for i in range(int(n/max_qubits))]
-    remainder = n % max_qubits
+def get_register_sizes(n):
+    register_sizes = [MAX_QUBITS for _ in range(int(n/MAX_QUBITS))]
+    remainder = n % MAX_QUBITS
     return register_sizes if remainder == 0 else register_sizes+[remainder]
 
 
+# Get number of bits from number value
 def num_bits(n):
     return math.floor(math.log(n, 2))+1
 
@@ -22,21 +25,37 @@ def bit_from_counts(counts):
     return [k for k, v in counts.items() if v == 1][0]
 
 
-def random_int(max, remote=False):
+# Enforce min value to have a visible string (ascii table) as we just allocate
+# memory which will then contain random bit values. To enforce min the value will be
+# adapted recursively.
+# @return Adapted random number.
+def cut_no_down_to_min(max_bound, min_bound, rand_no):
+    if rand_no < min_bound:
+        tmp_min = min_bound-rand_no
+        rand_no += cut_no_down_to_min(max_bound-rand_no, 0, random_int(tmp_min))
+
+    return rand_no
+
+
+# Generate random integer via qbits
+# @return Returns truly random integer
+def random_int(max_bound):
     bits = ''
-    n_bits = num_bits(max-1)
-    register_sizes = get_register_sizes(n_bits, MAX_QUBITS)
+    n_bits = num_bits(max_bound - 1)
+    register_sizes = get_register_sizes(n_bits)
     backend = BasicAer.get_backend("qasm_simulator")
-    # backend = "ibmqx4" if remote else "local_qasm_simulator"
 
     for x in register_sizes:
         q = QuantumRegister(x)
         c = ClassicalRegister(x)
         qc = QuantumCircuit(q, c)
 
+        # H-Gate, get into superposition
         qc.h(q)
+        # Measure value of superposition
         qc.measure(q, c)
 
+        # Execute quantum circuit on specific backend
         job_sim = execute(qc, backend, shots=1)
         sim_result = job_sim.result()
         counts = sim_result.get_counts(qc)
@@ -45,5 +64,23 @@ def random_int(max, remote=False):
     return int(bits, 2)
 
 
-result = random_int(15, False)
+# Parse/Cast generated integer into char value
+def random_char(rand_int):
+    return chr(rand_int)
+
+
+# Generate random string with specific length.
+# @return Generated String
+def random_str(length=10):
+    res_str = ""
+    for _ in range(length):
+        res_str += random_char(
+            cut_no_down_to_min(125, 33, random_int(125))
+        )
+
+    return res_str
+
+
+# Generate random string of specific length and print it afterwards
+result = random_str(30)
 print(result)
